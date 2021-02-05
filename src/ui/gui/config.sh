d753cc81bac51f51993a36a097c6dc4dbfae70bb
zsx-star <16603860375@163.com>
2021-02-05 18:47:33 +0800

    add ui

--- src/ui/gui/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh  
-  
-exe="smartcar" #发布的程序名称   这是我的可执行程序的名字
-des="/home/logistics_ws/src/ui/gui/lib/" #这个是用来存放依赖库的位置
-  
-deplist=$(ldd $exe | awk  '{if (match($3,"/")){ printf("%s "),$3 } }')  
-cp $deplist $des
