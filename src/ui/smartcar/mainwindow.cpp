7e11a7350ef6cf2b2a3f851973ef205ec0b1be57
zsx-star <16603860375@163.com>
2021-02-05 15:02:02 +0800

    temp

---
+++ src/ui/smartcar/mainwindow.cpp
@@ -0,0 +1,49 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_master_clicked()
+{
+   system("gnome-terminal -x bash -c 'sudo chmod 666 /dev/ttyUSB*;'&");
+}
+
+void MainWindow::on_gps_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch nuogeng driver.launch'&");
+}
+
+void MainWindow::on_record_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch driver path_recorder.launch'&");
+}
+
+void MainWindow::on_track_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch driver path_track.launch'&");
+}
+
+void MainWindow::on_livox_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch livox_ros_driver livox_lidar_rviz.launch'&");
+}
+
+void MainWindow::on_leishen_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch lslidar_c16_decoder lslidar_c16.launch'&");
+}
+
+void MainWindow::on_cluster_clicked()
+{
+    system("gnome-terminal -x bash -c 'source ~/logistics_ws/devel/setup.bash; roslaunch euclidean_cluster euclidean_cluster.launch'&");
+}
