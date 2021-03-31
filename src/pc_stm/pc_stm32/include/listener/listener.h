d404af811c2c58982467e77bbf73d866eb0be14f
zsx-star <16603860375@163.com>
2021-03-31 21:49:50 +0800

    add odom

--- src/pc_stm/pc_stm32/include/listener/listener.h
+++ src/pc_stm/pc_stm32/include/listener/listener.h
@@ -10,7 +10,20 @@
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 #include <logistics_msgs/RealState.h>
-#include <logistics_msgs/GoalState.h>
+#include <stdio.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Float32.h>
+#include <string>
+#include <vector>
+
+#define Wheel_Radius  0.25
+#define AXIS_DISTANCE 0.88  // 轴距
 
 class Listener
 {
@@ -18,6 +31,7 @@ public:
 	Listener();
 	~Listener();
 	
+	void run();
 	bool init();
 	void startReading();
 	void stopReading();
@@ -26,10 +40,11 @@ private:
 	void closeSerial();
 	void readSerialThread();
 	void parseIncomingData(uint8_t* buffer,size_t len);
-	
 	void parseFromStm(const unsigned char* buffer);
-	
 	bool sumCheck(const unsigned char* pkg_buffer, size_t pkg_len);
+	
+	void handle_speed_msg(uint8_t* buffer_data);
+	double generate_real_speed(double& temp1,double& temp2);
 	
 private:
 	ros::Publisher m_pub_state;
@@ -39,8 +54,20 @@ private:
 	boost::shared_ptr<boost::thread> m_read_thread_ptr;
 	bool m_reading_status;  //!< True if the read thread is running, false otherwise.
 	uint8_t * const m_pkg_buffer;
+//	logistics_msgs::RealState m_state;
+	
+	ros::Subscriber m_sub_state;
+	ros::Publisher odom_pub;
+	std::string car_state;
+	tf::TransformBroadcaster odom_broadcaster;
+	ros::Time current_time, last_time;
+	nav_msgs::Odometry odom;
 	logistics_msgs::RealState m_state;
-
+	
+	bool prase_flag_;
+	double x,y,th,vx,vy,vth;
+	double real_speed_left,real_speed_right,real_angle,real_speed;
+	double left_wheel_speed,right_wheel_speed,speed;
 };
 
 
--- src/pc_stm/pc_stm32/src/listen_stm32.cpp
+++ src/pc_stm/pc_stm32/src/listen_stm32.cpp
@@ -1,32 +1,36 @@
 #include"listener/listener.h"
-#include<logistics_msgs/RealState.h>
-#include<stdio.h>
 
 using namespace std;
-
+	
 Listener::Listener():				
 	m_reading_status(false),
+	prase_flag_(true),
 	m_pkg_buffer(new uint8_t[500])
 {
-}				
+}
 
 Listener::~Listener()
 {
 	this->closeSerial();
 	if(m_pkg_buffer!=NULL)
-		delete [] m_pkg_buffer;
-}            
+	delete [] m_pkg_buffer;
+}
 
+double Listener::generate_real_speed(double& temp1,double& temp2)
+{	
+	left_wheel_speed = 2 * M_PI * Wheel_Radius * temp1;
+	right_wheel_speed = 2 * M_PI * Wheel_Radius * temp2;
+	speed = (left_wheel_speed + right_wheel_speed) / 2;
+	return speed;
+}
 
-/*----------------------打开串口--------------------*/
-
-bool Listener::openSerial(const std::string& port,int baudrate)              // 打开串口
+bool Listener::openSerial(const std::string& port,int baudrate)
 {
 	try
 	{
 		m_serial_port = new serial::Serial(port,baudrate,serial::Timeout::simpleTimeout(10)); 
 	}
-	catch(serial::IOException& e)				//捕捉输入输出异常
+	catch(serial::IOException& e)
     {
         ROS_ERROR_STREAM("Unable to open port.");
         return -1;
@@ -49,8 +53,7 @@ bool Listener::openSerial(const std::string& port,int baudrate)              // 
 	return true;
 }     
 
-/*----------------------关闭串口-------------------*/
-void Listener::closeSerial()                                              // 关闭串口
+void Listener::closeSerial()
 {
 	if(m_serial_port!=NULL)
 	{
@@ -59,29 +62,22 @@ void Listener::closeSerial()                                              // 关
 	}
 }
 
-
-/*---------------------初始化---------------------*/
-
-bool Listener::init()  // 初始化
+bool Listener::init()
 {
 	ros::NodeHandle nh;
 	ros::NodeHandle nh_private("~");
-	
+	odom_pub = nh.advertise<nav_msgs::Odometry>("/wheel_odom", 50);
 	m_pub_state = nh.advertise<logistics_msgs::RealState>(nh_private.param<std::string>("car_state","/car_state"),10);
-
-	std::string port_name = nh_private.param<std::string>("port_name","/dev/pts/23");  // launch文件对应
-	
+	std::string port_name = nh_private.param<std::string>("port_name","/dev/pts/23");
 	int baudrate = nh_private.param<int>("baudrate",115200);
 	if(!openSerial(port_name,baudrate))
 		return false;
 	return true;
 }
 
-
-/*---------------开始从串口读取数据----------------*/
-void Listener::startReading()              // 开始读取
+void Listener::startReading()
 {
-	if(m_reading_status)   // 判断是否开始运行读取线程，此标志位初始时设置为false
+	if(m_reading_status)
 		return ;
 	m_read_thread_ptr = boost::shared_ptr<boost::thread>(new boost::thread(&Listener::readSerialThread,this));   // 智能指针赋值时 调用读取线程函数
 }
@@ -95,9 +91,9 @@ void Listener::readSerialThread()
 	
 	size_t get_len;
 		   
-	while(ros::ok() && m_reading_status)    // 不停的读取数据
+	while(ros::ok() && m_reading_status)
 	{	
-		try									// 异常处理
+		try
 		{
 			get_len = m_serial_port->read(raw_data_buf, Max_read_size);
 		}
@@ -107,25 +103,20 @@ void Listener::readSerialThread()
 			ros::Duration(0.01).sleep();
 			continue;
 		}
-		
 		if(get_len == 0) 
 		{
 			ros::Duration(0.01).sleep();
 			continue;
 		}
-		parseIncomingData(raw_data_buf, get_len);      // 解析数据
-		
+		parseIncomingData(raw_data_buf, get_len);
 		ros::Duration(0.01).sleep();
-		
 		//boost::this_thread::sleep(boost::posix_time::milliseconds(10));
 	}
-	
 	delete [] raw_data_buf;
 }
 
 void Listener::parseIncomingData(uint8_t* buffer,size_t len)
 {	
-//	std::cout << 2222222 << endl;
 	static unsigned char pkg_buffer[11];
 	static size_t pkg_buffer_index = 0; //数据包缓存定位索引
 	size_t pkg_len = 0;
@@ -151,31 +142,36 @@ void Listener::parseIncomingData(uint8_t* buffer,size_t len)
 			pkg_buffer[pkg_buffer_index++] = buffer[i];
 			if(pkg_buffer_index == 10)
 			{		
-//				std::cout << 333333 << endl;
 				pkg_len = pkg_buffer[3];
-//				std::cout << hex << pkg_buffer[11] << endl;
 				if((pkg_buffer[2] == 0x02) && sumCheck(pkg_buffer,pkg_len))    //0x02代表下位机向上位机发布的消息包id; 0xe0代表车速信号、制动信号、转角信号有效
 				if(1)
-					{
-//					std::cout << 4444444 << endl;
+				{
 					parseFromStm(pkg_buffer);
-					}					
+				}					
 				pkg_buffer_index = 0;
 			}
 		}
 	}
 }
 
-/*-------------------------求和校验-------------------*/
+void Listener::run()
+{
+	if(init())
+	{	
+		prase_flag_ = true;
+		startReading();
+		ROS_INFO("%s initial ok...",ros::this_node::getName().c_str());
+	}
+	ros::spin();
+}
+
 bool Listener::sumCheck(const unsigned char* pkg_buffer, size_t pkg_len)
 {	
-//	std::cout << 4444444 << endl;
 	size_t sum = 0;
 	for(int i = 2;i<pkg_len + 3;i++)
 	{	
 		sum = sum + pkg_buffer[i];
 	}
-//	std::cout << sum << endl;
 	if(sum == pkg_buffer[10])
 	{	
 		std::cout << sum << endl;
@@ -186,12 +182,10 @@ bool Listener::sumCheck(const unsigned char* pkg_buffer, size_t pkg_len)
 		return true;
 }
 
-/*---------------------解析当前车辆状态信息-----------*/
 void Listener::parseFromStm(const unsigned char* buffer)
 {	
 	m_state.header.stamp = ros::Time::now();
 	m_state.header.frame_id = "car_state";
-	
 	
 	m_state.real_speed_left = ((buffer[5] * 256 + buffer[6]) - 30000) * 0.01;
 	m_state.real_speed_right = ((buffer[7] * 256 + buffer[8]) - 30000) * 0.01;
@@ -199,17 +193,76 @@ void Listener::parseFromStm(const unsigned char* buffer)
 	m_state.real_brake = buffer[11] & 0xf0;
 	m_state.real_park  = buffer[11] & 0x0f;
 	
-	
 	m_pub_state.publish(m_state);
-//	std::cout << "上位机接收数据ok" << std::endl;
-
-//	printf("buffer[5]:%d\t buffer[6]:%d\t buffer[7]:%d\t buffer[8]:%d\n",buffer[5],buffer[6],buffer[7],buffer[8]);
 	printf("speed:%0.2f\t angle:%0.2f\n",m_state.real_speed,m_state.real_angle);
-//	std::cout << "real_speed:" << setm_state.real_speed << std::endl;
-//	std::cout << "real_angle：" << m_state.real_angle << std::endl;
+	
+	real_speed_left = m_state.real_speed_left;
+	real_speed_right = m_state.real_speed_right;
+	real_angle = m_state.real_angle;
+	real_speed = generate_real_speed(real_speed_left,real_speed_right);
+	
+	if(prase_flag_)
+	{
+		double x = 0.0;
+		double y = 0.0;
+		double th = 0.0;
+		last_time = ros::Time::now();
+		prase_flag_ = false;
+	}
+		double dt = (current_time - last_time).toSec();
+		vx = real_speed; // forward
+		vy = 0;
+		vth = (right_wheel_speed - left_wheel_speed) / AXIS_DISTANCE;  //左转角度为正
+		
+		double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
+		double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
+		double delta_th = vth * dt;
+		ros::Rate r(1.0);
+		ros::spinOnce();               // check for incoming messages
+		current_time = ros::Time::now();
+		
+		x += delta_x;
+		y += delta_y;
+		th += delta_th;
+		//里程计的偏航角转为四元数
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+		//首先,创建一个TransformStamped消息,通过tf发送;在current_time发布"odom"坐标到"base_link"的转换
+		geometry_msgs::TransformStamped odom_trans;
+		odom_trans.header.stamp = current_time;
+		odom_trans.header.frame_id = "odom";
+		odom_trans.child_frame_id = "base_link";
+
+		odom_trans.transform.translation.x = x;
+		odom_trans.transform.translation.y = y;
+		odom_trans.transform.translation.z = 0.0;
+		odom_trans.transform.rotation = odom_quat;
+
+		//send the transform
+		odom_broadcaster.sendTransform(odom_trans);
+
+		//next, we'll publish the odometry message over ROS
+		odom.header.frame_id = "odom";
+		odom.child_frame_id = "base_link";
+		odom.header.stamp = current_time;
+		//里程数据填充消息
+		//set the position
+		odom.pose.pose.position.x = x;
+		odom.pose.pose.position.y = y;
+		odom.pose.pose.position.z = 0.0;
+		odom.pose.pose.orientation = odom_quat;
+
+		//set the velocity
+		odom.twist.twist.linear.x = vx;
+		odom.twist.twist.linear.y = vy;
+		odom.twist.twist.angular.z = vth;
+
+		//publish the message
+		odom_pub.publish(odom);
+		ROS_DEBUG_STREAM("accumulation_x: " << x << "; accumulation_y: " << y <<"; accumulation_th: " << vth);
+		last_time = current_time;
+		r.sleep();
 }
 
-/*--------------------停止读取----------------------*/
 void Listener::stopReading()
 {
 	m_reading_status = false;
@@ -217,17 +270,7 @@ void Listener::stopReading()
 
 int main(int argc,char** argv)
 {
-//	std::cout << 0000000 << endl;
 	ros::init(argc,argv,"listener_node");
-	
 	Listener lis;
-	
-	if(lis.init())
-	{
-		lis.startReading();
-		ROS_INFO("%s initial ok...",ros::this_node::getName().c_str());
-	}
-	
-	ros::spin();
+	lis.run();
 }
-
