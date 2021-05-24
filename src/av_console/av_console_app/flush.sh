8225b2b59fa10e4893473f2107f105bc4495f4d7
CastielLiu <castiel_liu@outlook.com>
2021-05-25 04:27:18 +0800

    add av_op submodule

--- src/av_console/av_console_app/flush.sh
+++
@@ -1,8 +0,0 @@
-#rm -r cmd/
-rm app/av_console
-rm -r cmd/
-
-cp -r ../cmd .
-cp ../../../devel/lib/av_console/av_console app/
-
-
--- src/av_console/src/globalvariable.cpp
+++
@@ -1,8 +0,0 @@
-#include "globalvariables.hpp"
-/*全局变量*/
-
-/*@brief RosNodes: 存放ros nodes信息(launch)
- *       RosNodesArray: 存放多个launch文件信息
- */
-av_console::RosNodesArray g_rosNodesArray;
-
